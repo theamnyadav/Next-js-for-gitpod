b150ad6ff437e7ebd2c292d30c483a0fa89f5116
Aman Yadav <ekaman369@gmail.com>
2022-05-25 11:11:40 +0000

    II

--- vercelclone/pages/index.tsx
+++ vercelclone/pages/index.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center py-2">
       <Head>
         <title>Vercel Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-<h1>Vercel Clone</h1>
+<h1 className='text-3xl font-bold '>Vercel</h1>
     </div>
   )
 }
