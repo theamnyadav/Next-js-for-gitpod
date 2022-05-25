be6a2e6a0b399dc2e9ecc3d8eb831ddf95e2c2d1
Aman Yadav <ekaman369@gmail.com>
2022-05-25 10:56:31 +0000

    I

---
+++ vercelclone/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Vercel Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+<h1>Vercel Clone</h1>
+    </div>
+  )
+}
+
+export default Home
